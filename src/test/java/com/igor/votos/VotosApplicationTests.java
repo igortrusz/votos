a545561ab734504a9a6f688b3e70fd746e3b114e
Igor Trusz <trusz.igor@gmail.com>
2020-06-07 10:24:48 -0300

    Commit inicial

---
+++ src/test/java/com/igor/votos/VotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.votos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
